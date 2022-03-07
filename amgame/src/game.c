663bae4c1298b8d77925367fee97f37db9cbfb4a
tracer-nju <tracer@nju.edu.cn>
2022-03-08 00:39:12 +0800

    Linux VM-8-2-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:39:12 up 6 days,  8:22,  0 users,  load average: 0.12, 0.10, 0.03

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -4,15 +4,15 @@
 int main(const char *args) {
   ioe_init();
 
-  puts("mainargs = \"");
-  puts(args); // make run mainargs=xxx
-  puts("\"\n");
+  // puts("mainargs = \"");
+  // puts(args); // make run mainargs=xxx
+  // puts("\"\n");
 
-  splash();
+  // splash();
 
   puts("Press any key to see its key code...\n");
   while (1) {
-    print_key();
+    // print_key();
   }
   return 0;
 }

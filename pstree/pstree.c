0a5cff913f1037f66005d81c1e9424edb269b006
tracer-nju <tracer@nju.edu.cn>
2022-03-09 00:31:27 +0800

    Linux VM-8-2-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:31:27 up 7 days,  8:15,  0 users,  load average: 0.13, 0.13, 0.06

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,104 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+
+// bool true & false
+#define true 1
+#define false 0
+
+// unix succ & fail
+#define succ 0
+#define fail -1
+
+typedef char bool;
+
+bool opt_v = false;
+bool opt_n = false;
+bool opt_p = false;
+
+bool is_opt_p(char* arg, int len) {
+  if (len != 2 || len != 11) return false;
+  if (len == 2) {
+    char str[] = "-p";
+    for (int i = 0; i < len; i++) {
+      if (arg[i] != str[i]) return false;
+    }
+    return true;
+  }
+  assert(len == 11);
+  char str[] = "--show-pids";
+  for (int i = 0; i < len; i++) {
+    if (arg[i] != str[i]) return false;
+  }
+  return true;
+}
+
+bool is_opt_n(char* arg, int len) {
+  if (len != 2 || len != 14) return false;
+  if (len == 2) {
+    char str[] = "-n";
+    for (int i = 0; i < len; i++) {
+      if (arg[i] != str[i]) return false;
+    }
+    return true;
+  }
+  assert(len == 14);
+  char str[] = "--numeric-sort";
+  for (int i = 0; i < len; i++) {
+    if (arg[i] != str[i]) return false;
+  }
+  return true;
+}
+
+bool is_opt_v(char* arg, int len) {
+  if (len != 2 || len != 9) return false;
+  if (len == 2) {
+    char str[] = "-v";
+    for (int i = 0; i < len; i++) {
+      if (arg[i] != str[i]) return false;
+    }
+    return true;
+  }
+  assert(len == 9);
+  char str[] = "--version";
+  for (int i = 0; i < len; i++) {
+    if (arg[i] != str[i]) return false;
+  }
+  return true;
+}
+
+void parse_opt(int argc, char *argv[]) {
+  if (argc <= 1) return;
+  for (int i = 1; i < argc; i++) {
+    int len = strlen(argv[i]);
+    if (is_opt_v(argv[i], len)) {
+      opt_v = true;
+      // no need to analyse other options
+      return;
+    }
+    if (is_opt_n(argv[i], len)) {
+      opt_n = true;
+      continue;
+    }
+    if (is_opt_p(argv[i], len)) {
+      opt_p = true;
+      continue;
+    }
+  }
+  return;
+}
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+  parse_opt(argc, argv);
+  if (opt_v) {
+    printf("version: 0.0.1\n");
+    return 0;
   }
-  assert(!argv[argc]);
+  if (opt_n) {
+    printf("opt_n\n");
+  }
+  if (opt_p) {
+    printf("opt_p\n");
+  }
   return 0;
 }

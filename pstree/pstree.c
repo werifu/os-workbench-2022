ee54abb3c95825ceae88ceed6cae210c9d0dd584
tracer-nju <tracer@nju.edu.cn>
2022-03-09 00:08:46 +0800

    Linux VM-8-2-ubuntu 5.4.0-96-generic #109-Ubuntu SMP Wed Jan 12 16:49:16 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:08:46 up 7 days,  7:52,  0 users,  load average: 0.03, 0.05, 0.01

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
-  assert(FALSE);
+  assert(0);
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
